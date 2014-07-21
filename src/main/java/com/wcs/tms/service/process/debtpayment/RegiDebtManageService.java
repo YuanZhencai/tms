e20413ba45dc3abbd9623b00041b06b112b19513
Yuan Zhencai <yuanzhencai@wcs-global.com>
2014-07-21 16:48:14 +0800

    RegiCptiManager

--- src/main/java/com/wcs/tms/service/process/debtpayment/RegiDebtManageService.java
+++ src/main/java/com/wcs/tms/service/process/debtpayment/RegiDebtManageService.java
@@ -1,21 +1,43 @@
 package com.wcs.tms.service.process.debtpayment;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.primefaces.model.SortOrder;
 
 import com.wcs.base.service.EntityService;
+import com.wcs.base.util.StringUtils;
 import com.wcs.tms.model.Company;
 import com.wcs.tms.model.DebtQuota;
+import com.wcs.tms.model.ProcRegiCapital;
 import com.wcs.tms.model.ShareHolder;
+import com.wcs.tms.service.process.common.ProcessUtilMapService;
+import com.wcs.tms.service.report.regicapitalgeneral.RegicapitalGeneralRequestService;
 import com.wcs.tms.util.MessageUtils;
+import com.wcs.tms.view.process.common.entity.RegicapitalRequestVO;
 
 public class RegiDebtManageService {
+	
+	private Log log = LogFactory.getLog(RegiDebtManageService.class);
+	
+	@PersistenceContext(unitName = "pu")
+	private EntityManager em;
 	@Inject
 	EntityService entityService;
+	@Inject
+	ProcessUtilMapService processUtilMapService;
+	
 	  
 	public EntityService getEntityService() {
 		return entityService;
@@ -107,4 +129,97 @@ public class RegiDebtManageService {
 		debtQuota.setUnAccountAmount((Double)o5[1]);
 		return debtQuota;
 	}
+	
+	/**
+	 * 外债申请
+	 * @param filter
+	 * @return
+	 */
+	public HashMap<String, Object> findDebtRequests(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filter) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		try {
+		    Query query;
+			
+			StringBuilder jpql = new StringBuilder("select pdb from ProcDebtBorrow pdb join fetch pdb.company join fetch pdb.shareHolder where pdb.company.defunctInd = 'N' and pdb.company.status = 'Y' and pdb.shareHolder.defunctInd = 'N' ");
+			
+			//申请日期
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			if (null != filter.get("applyDateS")) {
+                String applyDate = sdf.format(filter.get("applyDateS"));
+                jpql.append(" and pdb.createdDatetime >= '" + applyDate + "' ");
+            }
+            if (null != filter.get("applyDateE")) {
+                String applyDate = sdf.format(filter.get("applyDateE"));
+                jpql.append(" and pdb.createdDatetime <= '" + applyDate + "' ");
+            }
+            
+            //流程编号
+            List<String> procInstIds = new ArrayList<String>();
+            if (null != filter.get("processNo") && !"".equals(filter.get("processNo"))) {
+                procInstIds = processUtilMapService.getFnIdsByTmsId(filter.get("processNo").toString());
+                if(procInstIds != null) {
+                    jpql.append(" and pdb.procInstId in (:procInstIds)");
+                }
+            }
+            
+			//流程状态
+            if (null != filter.get("processStatus") && !"".equals(filter.get("processStatus"))) {
+                jpql.append(" and pdb.processStatus = '" + filter.get("processStatus") + "' ");
+            }
+            
+            //股东名称
+            if (null != filter.get("shareHolderName") && !"".equals(filter.get("shareHolderName"))) {
+                jpql.append(" and pdb.shareHolderName = '" + filter.get("shareHolderName") + "' ");
+            }
+			
+			//公司
+            String companyId = (String) filter.get("companyId");
+			if (!StringUtils.isBlankOrNull(companyId)) { 
+			    jpql.append(" and pdb.company.id = " + companyId + " order by pdb.id");
+			    query = em.createQuery(jpql.toString());
+			}else {
+			    List<String> list = (List<String>) filter.get("companyIds");
+			    jpql.append(" and pdb.company.id in(:companyIds) order by pdb.id");
+                query = em.createQuery(jpql.toString()).setParameter("companyIds", list);
+			}
+			if(!procInstIds.isEmpty()) {
+                query.setParameter("procInstIds", procInstIds);
+            }
+			System.out.println("jpql : " + jpql);
+			/*int count = 1;
+			if (first == count && count != 0) {
+				query.setFirstResult(first - pageSize);
+			} else {
+				query.setFirstResult(first);
+			}
+			query.setMaxResults(pageSize);*/
+			int count = query.getResultList().size();
+            query.setFirstResult(first);
+            query.setMaxResults(pageSize);
+			List<ProcRegiCapital> procRegiCapitals = (List<ProcRegiCapital>) query.getResultList();
+			List<RegicapitalRequestVO> regicapitalRequestVOs = new ArrayList<RegicapitalRequestVO>();
+			if (!procRegiCapitals.isEmpty()) {
+				for (ProcRegiCapital pdb : procRegiCapitals) {
+//				    RegicapitalRequestVO rcVO = this.getRRVO(pdb);
+//				    regicapitalRequestVOs.add(rcVO);
+				}
+				map.put("count", count);
+				map.put("list", regicapitalRequestVOs);
+			}
+		} catch (Exception e) {
+			log.error("findShareHolderBySqLoad方法 错误信息" + e.getMessage());
+			e.printStackTrace();
+		}
+		return map;
+	}
+	
+	/**
+	 * 外债请款
+	 * @param filter
+	 * @return
+	 */
+	public HashMap<String, Object> findDebtRequestCashs(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, Object> filter) {
+		return null;
+	}
+	
 }
