a07192db4501b560e35720045783e2cad111d0c2
Yuan Zhencai <515265763@qq.com>
2014-07-22 10:59:48 +0800

    debt request manager

--- src/main/java/com/wcs/tms/service/report/debtborrow/DebtBorrowReportService.java
+++ src/main/java/com/wcs/tms/service/report/debtborrow/DebtBorrowReportService.java
@@ -11,29 +11,48 @@ import java.util.Map;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.primefaces.model.SortOrder;
 
 import com.wcs.base.service.EntityService;
 import com.wcs.base.service.LoginService;
+import com.wcs.base.util.DateUtil;
 import com.wcs.base.util.StringUtils;
 import com.wcs.common.controller.CommonBean;
 import com.wcs.common.filenet.pe.PEManager;
 import com.wcs.tms.conf.xml.ProcessXmlUtil;
 import com.wcs.tms.exception.ServiceException;
 import com.wcs.tms.message.ExceptionMessage;
+import com.wcs.tms.model.DebtContract;
 import com.wcs.tms.model.ProcDebtBorrow;
+import com.wcs.tms.model.ProcDebtPayment;
+import com.wcs.tms.service.process.common.ProcessUtilMapService;
 import com.wcs.tms.util.ProcessDefineUtil;
+import com.wcs.tms.view.process.common.entity.DebtBorrowRequestVO;
+import com.wcs.tms.view.process.common.entity.DebtRequestVo;
 
 import filenet.vw.api.VWException;
 import filenet.vw.api.VWLogElement;
 
 /**
- * <p>Project: tms</p>
- * <p>Description:外债申请明细表Service</p>
- * <p>Copyright (c) 2012 Wilmar Consultancy Services</p>
- * <p>All Rights Reserved.</p>
+ * <p>
+ * Project: tms
+ * </p>
+ * <p>
+ * Description:外债申请明细表Service
+ * </p>
+ * <p>
+ * Copyright (c) 2012 Wilmar Consultancy Services
+ * </p>
+ * <p>
+ * All Rights Reserved.
+ * </p>
+ * 
  * @author <a href="mailto:huhan@wcs-global.com">huhan</a>
  */
 @Stateless
@@ -41,6 +60,8 @@ public class DebtBorrowReportService implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private Log log = LogFactory.getLog(DebtBorrowReportService.class);
+	@PersistenceContext(unitName = "pu")
+	private EntityManager em;
 	@Inject
 	EntityService entityService;
 	@Inject
@@ -49,9 +70,12 @@ public class DebtBorrowReportService implements Serializable {
 	LoginService loginService;
 	@Inject
 	CommonBean dictBean;
+	@Inject
+	ProcessUtilMapService processUtilMapService;
 
 	/**
 	 * 查询外债申请明细
+	 * 
 	 * @param conditionMap
 	 * @return
 	 * @throws ServiceException
@@ -70,7 +94,7 @@ public class DebtBorrowReportService implements Serializable {
 				procIds.add(le.getWorkObjectNumber());
 			}
 		} catch (Exception e) {
-		    log.error("getProcDebtBorrowList方法中前一方法 查询外债申请明细出现异常" , e);
+			log.error("getProcDebtBorrowList方法中前一方法 查询外债申请明细出现异常", e);
 			throw new ServiceException(ExceptionMessage.FN_CONNECT, e);
 		}
 		try {
@@ -79,7 +103,7 @@ public class DebtBorrowReportService implements Serializable {
 			// 计算借款时间和还款时间
 			this.procDebtBorrowFilter(dbList, les);
 		} catch (Exception e) {
-			log.error("getProcDebtBorrowList方法中后一方法 查询外债申请明细出现异常" , e);
+			log.error("getProcDebtBorrowList方法中后一方法 查询外债申请明细出现异常", e);
 			throw new ServiceException(ExceptionMessage.FN_CONNECT, e);
 		}
 		return dbList;
@@ -87,6 +111,7 @@ public class DebtBorrowReportService implements Serializable {
 
 	/**
 	 * 得到已完成的外债流程的过滤器
+	 * 
 	 * @param conditionMap
 	 * @return
 	 */
@@ -112,6 +137,7 @@ public class DebtBorrowReportService implements Serializable {
 
 	/**
 	 * 流程实例ID查询外债实体
+	 * 
 	 * @param wobNoList
 	 * @return
 	 */
@@ -150,7 +176,7 @@ public class DebtBorrowReportService implements Serializable {
 				}
 			}
 		} catch (Exception e) {
-		    log.error("findProcDebtBorrowListByWob方法 流程实例ID查询外债实体出现异常" , e);
+			log.error("findProcDebtBorrowListByWob方法 流程实例ID查询外债实体出现异常", e);
 			throw new ServiceException(ExceptionMessage.FN_CONNECT, e);
 		}
 		return resultList;
@@ -158,9 +184,10 @@ public class DebtBorrowReportService implements Serializable {
 
 	/**
 	 * 计算贷款时间(不知道这个方法有啥作用)
+	 * 
 	 * @param dbList
 	 * @param les
-	 * @throws VWException 
+	 * @throws VWException
 	 * @throws Exception
 	 */
 	private void procDebtBorrowFilter(List<ProcDebtBorrow> dbList, List<VWLogElement> les) throws VWException {
@@ -194,5 +221,101 @@ public class DebtBorrowReportService implements Serializable {
 				}
 			}
 		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public HashMap<String, Object> findDebtBorrowRequestDetail(int first, int pageSize, String sortField, SortOrder sortOrder,
+			Map<String, Object> conditionMap) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		Query query;
+		try {
+			StringBuilder jpql = new StringBuilder("select db from ProcDebtBorrow db join fetch db.company where db.defunctInd='N' ");
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			if (null != conditionMap.get("creditStartS")) {
+				String start = sdf.format(conditionMap.get("startDateS"));
+				jpql.append(" and db.corpAuditLis >= '" + start + "' ");
+			}
+			if (null != conditionMap.get("startDateE")) {
+				String start = sdf.format(conditionMap.get("startDateE"));
+				jpql.append(" and db.corpAuditLis <= '" + start + "' ");
+			}
+			if (null != conditionMap.get("endDateS")) {
+				String start = sdf.format(conditionMap.get("endDateS"));
+				jpql.append(" and db.corpAuditLie >= '" + start + "' ");
+			}
+			if (null != conditionMap.get("endDateE")) {
+				String start = sdf.format(conditionMap.get("endDateE"));
+				jpql.append(" and db.corpAuditLie <= '" + start + "' ");
+			}
+			// 流程编号
+			List<String> procInstIds = new ArrayList<String>();
+			if (null != conditionMap.get("processNo") && !"".equals(conditionMap.get("processNo"))) {
+				procInstIds = processUtilMapService.getFnIdsByTmsId(conditionMap.get("processNo").toString());
+				if (procInstIds != null) {
+					jpql.append(" and pdp.procInstId in (:procInstIds)");
+				}
+			}
+
+			String companyId = (String) conditionMap.get("companyId");
+			if (!StringUtils.isBlankOrNull(companyId)) {
+				jpql.append(" and db.company.id = " + conditionMap.get("companyId"));
+				query = em.createQuery(jpql.toString());
+			} else {
+				List<String> companyIds = (List<String>) conditionMap.get("companyIds");
+				if (companyIds != null && !companyIds.isEmpty()) {
+					jpql.append(" and db.company.id in (:companyIds)");
+					query = em.createQuery(jpql.toString()).setParameter("companyIds", companyIds);
+				}
+			}
+			if (!procInstIds.isEmpty()) {
+				query.setParameter("procInstIds", procInstIds);
+			}
+			int count = query.getResultList().size();
+			query.setFirstResult(first);
+			query.setMaxResults(pageSize);
+			List<ProcDebtBorrow> procDebtBorrows = (List<ProcDebtBorrow>) query.getResultList();
+			List<DebtRequestVo> debtRequestVos = new ArrayList<DebtRequestVo>();
+			for (ProcDebtBorrow pdb : procDebtBorrows) {
+				DebtRequestVo debtRequestVo = this.getDebtRequestVo(pdb);
+				debtRequestVos.add(debtRequestVo);
+			}
+			map.put("count", count);
+			map.put("list", debtRequestVos);
+
+		} catch (Exception e) {
+			log.error("findDebtBorrowRequestDetail方法 错误信息" + e.getMessage());
+			e.printStackTrace();
+		}
+		return map;
+	}
+
+	/**
+	 * @param pdb
+	 * @return
+	 */
+	public DebtRequestVo getDebtRequestVo(ProcDebtBorrow pdb) {
+		DebtRequestVo debtRequestVo = new DebtRequestVo(pdb);
+		
+		// 流程编号
+		debtRequestVo.setProcessNo(processUtilMapService.getTmsIdByFnId(pdb.getProcInstId()));
+		// 申请金额
+		debtRequestVo.setRequestMoney(pdb.getCorpAudit()== null ? "/" : pdb.getCorpAudit().toString());
+		// 币别
+		debtRequestVo.setCurrency(pdb.getCorpAuditCu());
+		// 是否到账
+		/** 外债合同_主数据DEBT_CONTRACT **/
+		// 外债合同 = 合同编号 + 出资方 + 开始-结束日期 + 利率
+		DebtContract debtContract = entityService.find(DebtContract.class, pdb.getDebtContractId());
+		String debtContractStr = debtContract.getDebtContractNo() + " + " + debtContract.getShareHolder().getShareHolderName() + " + " + DateUtil.convertDateToString(debtContract.getContractStartDate(), "yyyy-MM-dd") + "-" + DateUtil.convertDateToString(debtContract.getContractEndDate(), "yyyy-MM-dd") + " + " + (debtContract.getApprovalRate() == null ? "" : debtContract.getApprovalRate());
+		debtRequestVo.setDebtContract(debtContractStr);
+		// 外债合同金额
+		debtRequestVo.setDebtContractFunds(debtContract.getDebtContractFunds() == null ? "/" : debtContract.getDebtContractFunds().toString());
+		// 未请款金额 = 外债合同金额 - 未请款金额
+		Double noAppliedFunds = (debtContract.getDebtContractFunds() == null ? 0 : debtContract.getDebtContractFunds()) - (debtContract.getAppliedFunds() == null ? 0 : debtContract.getAppliedFunds());
+		debtRequestVo.setNoAppliedFunds(noAppliedFunds == 0 ? "/" : noAppliedFunds.toString());
+		// 已请款金额
+		debtRequestVo.setApplyFunds(debtContract.getAppliedFunds() == null ? "/" : debtContract.getAppliedFunds().toString());
+
+		return debtRequestVo;
 	}
 }
---
+++ src/main/java/com/wcs/tms/view/process/common/entity/DebtRequestVo.java
@@ -0,0 +1,866 @@
+package com.wcs.tms.view.process.common.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import com.wcs.common.controller.helper.IdModel;
+import com.wcs.tms.model.Company;
+import com.wcs.tms.model.ProcDebtBorrow;
+import com.wcs.tms.model.ShareHolder;
+
+public class DebtRequestVo extends IdModel implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+	private ProcDebtBorrow debtBorrow;
+
+	private Company company;
+
+	/*** 资金提供股东 *******/
+	private ShareHolder shareHolder;
+
+	private String processNo;
+	
+	private String procInstId;
+
+	private String companyName;
+
+	private String companyEn;
+
+	private Double investBalance;
+
+	private String investBalanceCu;
+
+	private Double bebtInPlace;
+
+	private String bebtInPlaceCu;
+
+	private Double shBorrow;
+
+	private String shBorrowCu;
+
+	private Double shBorrowLi;
+
+	private Double shBorrowRa;
+
+	private Double fornBebt;
+
+	private String fornBebtCu;
+
+	private Double fornBebtLi;
+
+	private Double fornBebtRa;
+
+	/****** 可用外债额度 改为 可用投注差 ***************/
+	private Double availbBebt;
+
+	private String availbBebtCu;
+
+	private String thisShBorrowSe;
+
+	private Double thisShBorrow;
+
+	private String thisShBorrowCu;
+
+	private Double thisShBorrowLi;
+
+	/****** 本次股东借款起始年月 ***************/
+	private Date thisShBorrowLis;
+
+	/****** 本次股东借款结束年月 ***************/
+	private Date thisShBorrowLie;
+
+	private String thisShBorrowRa;
+
+	/** 外债提供方 (去除掉) start ****/
+	private Double bebtProvider;
+
+	private String bebtProviderCu;
+
+	private Double bebtProviderLi;
+
+	private Double bebtProviderRa;
+	/** 外债提供方 (去除掉) end ****/
+
+	private String use;
+
+	private String afceFlag = "N";
+
+	private Double afceSign;
+
+	private String afceSignCu;
+
+	/** AFCE折算汇率 ****/
+	private Double afceExcRate;
+
+	/** AFCE签批情况(折算值) ****/
+	private Double afceSignExc;
+
+	/** AFCE已付款金额 ****/
+	private Double afcePaid;
+
+	/** AFCE已付款金额币别 ****/
+	private String afcePaidCu;
+
+	private Double corpAudit;
+
+	private String corpAuditCu;
+
+	private Double corpAuditLi;
+
+	/****** 集团项目审批期限起始年月 ***************/
+	private Date corpAuditLis;
+
+	/****** 集团项目审批期限结束年月 ***************/
+	private Date corpAuditLie;
+
+	private String corpAuditRa;
+
+	/** 备注(小股东是否按比例提供资金) ****/
+	private String Memo;
+
+	/** 还款方式及来源 ****/
+	private String payBackMemo;
+
+	/** 资金提供方类型 ****/
+	private String providerType;
+
+	/** 资金提供公司KEY ****/
+	private String providerKey;
+
+	/** 折算汇率 ****/
+	private Double exchangeRate;
+
+	/** 保存PE的备注 */
+	private String peMemo;
+	// Constructors
+
+	/** 贷款时间 */
+	private Date borrowDate;
+
+	/** 还款时间 */
+	private Date payBackDate;
+
+	/** 资金提供方名称 ****/
+	private String providerName;
+
+	/** 2014-07-04 add by liushengbin 新加字段 **/
+
+	/*** 外债期限类型 **/
+	private String bebtDeadlineType;
+	/*** 申请类型 **/
+	private String applyType;
+	/*** 展期原外债合同主数据ID 关联 ‘外债合同_主数据(DEBT_CONTRACT)’表 **/
+	private Long debtContractId;
+	/** 当前节点 **/
+	private String currentNode;
+	/** 流程状态 **/
+	private String processStatus;
+	
+	/** 外债合同_主数据DEBT_CONTRACT**/
+    // 外债合同 = 合同编号+出资方+开始-结束日期+利率
+    private String debtContract; 
+    // 外债合同金额
+    private String debtContractFunds;
+    // 未请款金额
+    private String noAppliedFunds;
+    // 请款金额
+    private String applyFunds;
+    // 到账金额
+    private String receivedFunds;
+    
+    // 申请金额
+    private String requestMoney;
+    
+    // 币别
+    private String currency;
+
+	public DebtRequestVo() {
+	}
+
+	public DebtRequestVo(ProcDebtBorrow debtBorrow) {
+		this.debtBorrow = debtBorrow;
+		
+		this.company = debtBorrow.getCompany();
+
+		/*** 资金提供股东 *******/
+		this.shareHolder = debtBorrow.getShareHolder();
+
+		this.procInstId = debtBorrow.getProcInstId();
+
+		this.companyName = debtBorrow.getCompanyName();
+
+		this.companyEn = debtBorrow.getCompanyEn();
+
+		this.investBalance = debtBorrow.getInvestBalance();
+
+		this.investBalanceCu = debtBorrow.getInvestBalanceCu();
+
+		this.bebtInPlace = debtBorrow.getBebtInPlace();
+
+		this.bebtInPlaceCu = debtBorrow.getBebtInPlaceCu();
+
+		this.shBorrow = debtBorrow.getShBorrow();
+
+		this.shBorrowCu = debtBorrow.getShBorrowCu();
+
+		this.shBorrowLi = debtBorrow.getShBorrowLi();
+
+		this.shBorrowRa = debtBorrow.getShBorrowRa();
+
+		this.fornBebt = debtBorrow.getFornBebt();
+
+		this.fornBebtCu = debtBorrow.getFornBebtCu();
+
+		this.fornBebtLi = debtBorrow.getFornBebtLi();
+
+		this.fornBebtRa = debtBorrow.getFornBebtRa();
+
+		/****** 可用外债额度 改为 可用投注差 ***************/
+		this.availbBebt = debtBorrow.getAvailbBebt();
+
+		this.availbBebtCu = debtBorrow.getAvailbBebtCu();
+
+		this.thisShBorrowSe = debtBorrow.getThisShBorrowCu();
+
+		this.thisShBorrow = debtBorrow.getThisShBorrow();
+
+		this.thisShBorrowCu = debtBorrow.getThisShBorrowCu();
+
+		this.thisShBorrowLi = debtBorrow.getThisShBorrowLi();
+
+		/****** 本次股东借款起始年月 ***************/
+		this.thisShBorrowLis = debtBorrow.getThisShBorrowLis();
+
+		/****** 本次股东借款结束年月 ***************/
+		this.thisShBorrowLie = debtBorrow.getThisShBorrowLie();
+
+		this.thisShBorrowRa = debtBorrow.getThisShBorrowRa();
+
+		/** 外债提供方 (去除掉) start ****/
+		this.bebtProvider = debtBorrow.getBebtProvider();
+
+		this.bebtProviderCu = debtBorrow.getBebtProviderCu();
+
+		this.bebtProviderLi = debtBorrow.getBebtProviderLi();
+
+		this.bebtProviderRa = debtBorrow.getBebtProviderRa();
+		/** 外债提供方 (去除掉) end ****/
+
+		this.use = debtBorrow.getUse();
+
+		this.afceFlag = debtBorrow.getAfceFlag();
+
+		this.afceSign = debtBorrow.getAfceSign();
+
+		this.afceSignCu = debtBorrow.getAfceSignCu();
+
+		/** AFCE折算汇率 ****/
+		this.afceExcRate = debtBorrow.getAfceExcRate();
+
+		/** AFCE签批情况(折算值) ****/
+		this.afceSignExc = debtBorrow.getAfceSignExc();
+
+		/** AFCE已付款金额 ****/
+		this.afcePaid = debtBorrow.getAfcePaid();
+
+		/** AFCE已付款金额币别 ****/
+		this.afcePaidCu = debtBorrow.getAfcePaidCu();
+
+		this.corpAudit = debtBorrow.getCorpAudit();
+
+		this.corpAuditCu = debtBorrow.getCorpAuditCu();
+
+		this.corpAuditLi = debtBorrow.getCorpAuditLi();
+
+		/****** 集团项目审批期限起始年月 ***************/
+		this.corpAuditLis = debtBorrow.getCorpAuditLis();
+
+		/****** 集团项目审批期限结束年月 ***************/
+		this.corpAuditLie = debtBorrow.getCorpAuditLie();
+
+		this.corpAuditRa = debtBorrow.getCorpAuditRa();
+
+		/** 备注(小股东是否按比例提供资金) ****/
+		this.Memo = debtBorrow.getMemo();
+
+		/** 还款方式及来源 ****/
+		this.payBackMemo = debtBorrow.getPayBackMemo();
+
+		/** 资金提供方类型 ****/
+		this.providerType = debtBorrow.getProviderType();
+
+		/** 资金提供公司KEY ****/
+		this.providerKey = debtBorrow.getProviderKey();
+
+		/** 折算汇率 ****/
+		this.exchangeRate = debtBorrow.getExchangeRate();
+
+		/** 保存PE的备注 */
+		this.peMemo = debtBorrow.getPeMemo();
+		// Constructors
+
+		/** 贷款时间 */
+		this.borrowDate = debtBorrow.getBorrowDate();
+
+		/** 还款时间 */
+		this.payBackDate = debtBorrow.getPayBackDate();
+
+		/** 资金提供方名称 ****/
+		this.providerName = debtBorrow.getProviderName();
+
+		/** 2014-07-04 add by liushengbin 新加字段 **/
+
+		/*** 外债期限类型 **/
+		this.bebtDeadlineType = debtBorrow.getBebtDeadlineType();
+		/*** 申请类型 **/
+		this.applyType = debtBorrow.getApplyType();
+		/*** 展期原外债合同主数据ID 关联 ‘外债合同_主数据(DEBT_CONTRACT)’表 **/
+		this.debtContractId = debtBorrow.getDebtContractId();
+		/** 当前节点 **/
+		this.currentNode = debtBorrow.getCurrentNode();
+		/** 流程状态 **/
+		this.processStatus = debtBorrow.getProcessStatus();
+	}
+
+	public ProcDebtBorrow getDebtBorrow() {
+		return debtBorrow;
+	}
+
+	public void setDebtBorrow(ProcDebtBorrow debtBorrow) {
+		this.debtBorrow = debtBorrow;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+
+	public ShareHolder getShareHolder() {
+		return shareHolder;
+	}
+
+	public void setShareHolder(ShareHolder shareHolder) {
+		this.shareHolder = shareHolder;
+	}
+
+	public String getProcInstId() {
+		return procInstId;
+	}
+
+	public void setProcInstId(String procInstId) {
+		this.procInstId = procInstId;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+	public String getCompanyEn() {
+		return companyEn;
+	}
+
+	public void setCompanyEn(String companyEn) {
+		this.companyEn = companyEn;
+	}
+
+	public Double getInvestBalance() {
+		return investBalance;
+	}
+
+	public void setInvestBalance(Double investBalance) {
+		this.investBalance = investBalance;
+	}
+
+	public String getInvestBalanceCu() {
+		return investBalanceCu;
+	}
+
+	public void setInvestBalanceCu(String investBalanceCu) {
+		this.investBalanceCu = investBalanceCu;
+	}
+
+	public Double getBebtInPlace() {
+		return bebtInPlace;
+	}
+
+	public void setBebtInPlace(Double bebtInPlace) {
+		this.bebtInPlace = bebtInPlace;
+	}
+
+	public String getBebtInPlaceCu() {
+		return bebtInPlaceCu;
+	}
+
+	public void setBebtInPlaceCu(String bebtInPlaceCu) {
+		this.bebtInPlaceCu = bebtInPlaceCu;
+	}
+
+	public Double getShBorrow() {
+		return shBorrow;
+	}
+
+	public void setShBorrow(Double shBorrow) {
+		this.shBorrow = shBorrow;
+	}
+
+	public String getShBorrowCu() {
+		return shBorrowCu;
+	}
+
+	public void setShBorrowCu(String shBorrowCu) {
+		this.shBorrowCu = shBorrowCu;
+	}
+
+	public Double getShBorrowLi() {
+		return shBorrowLi;
+	}
+
+	public void setShBorrowLi(Double shBorrowLi) {
+		this.shBorrowLi = shBorrowLi;
+	}
+
+	public Double getShBorrowRa() {
+		return shBorrowRa;
+	}
+
+	public void setShBorrowRa(Double shBorrowRa) {
+		this.shBorrowRa = shBorrowRa;
+	}
+
+	public Double getFornBebt() {
+		return fornBebt;
+	}
+
+	public void setFornBebt(Double fornBebt) {
+		this.fornBebt = fornBebt;
+	}
+
+	public String getFornBebtCu() {
+		return fornBebtCu;
+	}
+
+	public void setFornBebtCu(String fornBebtCu) {
+		this.fornBebtCu = fornBebtCu;
+	}
+
+	public Double getFornBebtLi() {
+		return fornBebtLi;
+	}
+
+	public void setFornBebtLi(Double fornBebtLi) {
+		this.fornBebtLi = fornBebtLi;
+	}
+
+	public Double getFornBebtRa() {
+		return fornBebtRa;
+	}
+
+	public void setFornBebtRa(Double fornBebtRa) {
+		this.fornBebtRa = fornBebtRa;
+	}
+
+	public Double getAvailbBebt() {
+		return availbBebt;
+	}
+
+	public void setAvailbBebt(Double availbBebt) {
+		this.availbBebt = availbBebt;
+	}
+
+	public String getAvailbBebtCu() {
+		return availbBebtCu;
+	}
+
+	public void setAvailbBebtCu(String availbBebtCu) {
+		this.availbBebtCu = availbBebtCu;
+	}
+
+	public String getThisShBorrowSe() {
+		return thisShBorrowSe;
+	}
+
+	public void setThisShBorrowSe(String thisShBorrowSe) {
+		this.thisShBorrowSe = thisShBorrowSe;
+	}
+
+	public Double getThisShBorrow() {
+		return thisShBorrow;
+	}
+
+	public void setThisShBorrow(Double thisShBorrow) {
+		this.thisShBorrow = thisShBorrow;
+	}
+
+	public String getThisShBorrowCu() {
+		return thisShBorrowCu;
+	}
+
+	public void setThisShBorrowCu(String thisShBorrowCu) {
+		this.thisShBorrowCu = thisShBorrowCu;
+	}
+
+	public Double getThisShBorrowLi() {
+		return thisShBorrowLi;
+	}
+
+	public void setThisShBorrowLi(Double thisShBorrowLi) {
+		this.thisShBorrowLi = thisShBorrowLi;
+	}
+
+	public Date getThisShBorrowLis() {
+		return thisShBorrowLis;
+	}
+
+	public void setThisShBorrowLis(Date thisShBorrowLis) {
+		this.thisShBorrowLis = thisShBorrowLis;
+	}
+
+	public Date getThisShBorrowLie() {
+		return thisShBorrowLie;
+	}
+
+	public void setThisShBorrowLie(Date thisShBorrowLie) {
+		this.thisShBorrowLie = thisShBorrowLie;
+	}
+
+	public String getThisShBorrowRa() {
+		return thisShBorrowRa;
+	}
+
+	public void setThisShBorrowRa(String thisShBorrowRa) {
+		this.thisShBorrowRa = thisShBorrowRa;
+	}
+
+	public Double getBebtProvider() {
+		return bebtProvider;
+	}
+
+	public void setBebtProvider(Double bebtProvider) {
+		this.bebtProvider = bebtProvider;
+	}
+
+	public String getBebtProviderCu() {
+		return bebtProviderCu;
+	}
+
+	public void setBebtProviderCu(String bebtProviderCu) {
+		this.bebtProviderCu = bebtProviderCu;
+	}
+
+	public Double getBebtProviderLi() {
+		return bebtProviderLi;
+	}
+
+	public void setBebtProviderLi(Double bebtProviderLi) {
+		this.bebtProviderLi = bebtProviderLi;
+	}
+
+	public Double getBebtProviderRa() {
+		return bebtProviderRa;
+	}
+
+	public void setBebtProviderRa(Double bebtProviderRa) {
+		this.bebtProviderRa = bebtProviderRa;
+	}
+
+	public String getUse() {
+		return use;
+	}
+
+	public void setUse(String use) {
+		this.use = use;
+	}
+
+	public String getAfceFlag() {
+		return afceFlag;
+	}
+
+	public void setAfceFlag(String afceFlag) {
+		this.afceFlag = afceFlag;
+	}
+
+	public Double getAfceSign() {
+		return afceSign;
+	}
+
+	public void setAfceSign(Double afceSign) {
+		this.afceSign = afceSign;
+	}
+
+	public String getAfceSignCu() {
+		return afceSignCu;
+	}
+
+	public void setAfceSignCu(String afceSignCu) {
+		this.afceSignCu = afceSignCu;
+	}
+
+	public Double getAfceExcRate() {
+		return afceExcRate;
+	}
+
+	public void setAfceExcRate(Double afceExcRate) {
+		this.afceExcRate = afceExcRate;
+	}
+
+	public Double getAfceSignExc() {
+		return afceSignExc;
+	}
+
+	public void setAfceSignExc(Double afceSignExc) {
+		this.afceSignExc = afceSignExc;
+	}
+
+	public Double getAfcePaid() {
+		return afcePaid;
+	}
+
+	public void setAfcePaid(Double afcePaid) {
+		this.afcePaid = afcePaid;
+	}
+
+	public String getAfcePaidCu() {
+		return afcePaidCu;
+	}
+
+	public void setAfcePaidCu(String afcePaidCu) {
+		this.afcePaidCu = afcePaidCu;
+	}
+
+	public Double getCorpAudit() {
+		return corpAudit;
+	}
+
+	public void setCorpAudit(Double corpAudit) {
+		this.corpAudit = corpAudit;
+	}
+
+	public String getCorpAuditCu() {
+		return corpAuditCu;
+	}
+
+	public void setCorpAuditCu(String corpAuditCu) {
+		this.corpAuditCu = corpAuditCu;
+	}
+
+	public Double getCorpAuditLi() {
+		return corpAuditLi;
+	}
+
+	public void setCorpAuditLi(Double corpAuditLi) {
+		this.corpAuditLi = corpAuditLi;
+	}
+
+	public Date getCorpAuditLis() {
+		return corpAuditLis;
+	}
+
+	public void setCorpAuditLis(Date corpAuditLis) {
+		this.corpAuditLis = corpAuditLis;
+	}
+
+	public Date getCorpAuditLie() {
+		return corpAuditLie;
+	}
+
+	public void setCorpAuditLie(Date corpAuditLie) {
+		this.corpAuditLie = corpAuditLie;
+	}
+
+	public String getCorpAuditRa() {
+		return corpAuditRa;
+	}
+
+	public void setCorpAuditRa(String corpAuditRa) {
+		this.corpAuditRa = corpAuditRa;
+	}
+
+	public String getMemo() {
+		return Memo;
+	}
+
+	public void setMemo(String memo) {
+		Memo = memo;
+	}
+
+	public String getPayBackMemo() {
+		return payBackMemo;
+	}
+
+	public void setPayBackMemo(String payBackMemo) {
+		this.payBackMemo = payBackMemo;
+	}
+
+	public String getProviderType() {
+		return providerType;
+	}
+
+	public void setProviderType(String providerType) {
+		this.providerType = providerType;
+	}
+
+	public String getProviderKey() {
+		return providerKey;
+	}
+
+	public void setProviderKey(String providerKey) {
+		this.providerKey = providerKey;
+	}
+
+	public Double getExchangeRate() {
+		return exchangeRate;
+	}
+
+	public void setExchangeRate(Double exchangeRate) {
+		this.exchangeRate = exchangeRate;
+	}
+
+	public String getPeMemo() {
+		return peMemo;
+	}
+
+	public void setPeMemo(String peMemo) {
+		this.peMemo = peMemo;
+	}
+
+	public Date getBorrowDate() {
+		return borrowDate;
+	}
+
+	public void setBorrowDate(Date borrowDate) {
+		this.borrowDate = borrowDate;
+	}
+
+	public Date getPayBackDate() {
+		return payBackDate;
+	}
+
+	public void setPayBackDate(Date payBackDate) {
+		this.payBackDate = payBackDate;
+	}
+
+	public String getProviderName() {
+		return providerName;
+	}
+
+	public void setProviderName(String providerName) {
+		this.providerName = providerName;
+	}
+
+	public String getBebtDeadlineType() {
+		return bebtDeadlineType;
+	}
+
+	public void setBebtDeadlineType(String bebtDeadlineType) {
+		this.bebtDeadlineType = bebtDeadlineType;
+	}
+
+	public String getApplyType() {
+		return applyType;
+	}
+
+	public void setApplyType(String applyType) {
+		this.applyType = applyType;
+	}
+
+	public Long getDebtContractId() {
+		return debtContractId;
+	}
+
+	public void setDebtContractId(Long debtContractId) {
+		this.debtContractId = debtContractId;
+	}
+
+	public String getCurrentNode() {
+		return currentNode;
+	}
+
+	public void setCurrentNode(String currentNode) {
+		this.currentNode = currentNode;
+	}
+
+	public String getProcessStatus() {
+		return processStatus;
+	}
+
+	public void setProcessStatus(String processStatus) {
+		this.processStatus = processStatus;
+	}
+
+	public String getProcessNo() {
+		return processNo;
+	}
+
+	public void setProcessNo(String processNo) {
+		this.processNo = processNo;
+	}
+
+	public String getDebtContract() {
+		return debtContract;
+	}
+
+	public void setDebtContract(String debtContract) {
+		this.debtContract = debtContract;
+	}
+
+	public String getDebtContractFunds() {
+		return debtContractFunds;
+	}
+
+	public void setDebtContractFunds(String debtContractFunds) {
+		this.debtContractFunds = debtContractFunds;
+	}
+
+	public String getNoAppliedFunds() {
+		return noAppliedFunds;
+	}
+
+	public void setNoAppliedFunds(String noAppliedFunds) {
+		this.noAppliedFunds = noAppliedFunds;
+	}
+
+	public String getApplyFunds() {
+		return applyFunds;
+	}
+
+	public void setApplyFunds(String applyFunds) {
+		this.applyFunds = applyFunds;
+	}
+
+	public String getReceivedFunds() {
+		return receivedFunds;
+	}
+
+	public void setReceivedFunds(String receivedFunds) {
+		this.receivedFunds = receivedFunds;
+	}
+
+	public String getRequestMoney() {
+		return requestMoney;
+	}
+
+	public void setRequestMoney(String requestMoney) {
+		this.requestMoney = requestMoney;
+	}
+
+	public String getCurrency() {
+		return currency;
+	}
+
+	public void setCurrency(String currency) {
+		this.currency = currency;
+	}
+
+}
